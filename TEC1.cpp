aa0fb24596e86ea9aec13c3453969e7ada40404a
dduarteCE <daduarte@estudiantec.cr>
2023-03-02 12:47:30 -0600

    Creación de clases Node y List

---
+++ TEC1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Node{
+    public:
+     int data;
+     Node * next;
+     Node(){
+        data = 0;
+        next = NULL;
+     }
+     Node (int data){
+        this->data = data;
+        this->next = NULL;
+     }
+     void setData(int x){
+        data = x;
+     }
+     int getData(){
+        return data;
+     }
+};
+class List{
+    public:
+    Node* head;
+    List(){
+        head = NULL;
+    }
+    void setHead(Node* head){
+        this->head = head;
+    }
+    int getHead(){
+        return head->data;
+    }
+    void insert(int data){
+        Node* newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+        }
+        else{
+            newNode->next = this->head;
+            this->head=newNode;
+        }
+    }
+};
+int main(){
+    return 0;
+}
