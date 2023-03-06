69f5c266d2213154ebb38be723cdac90eac8363b
EstDan09 <esecaidavargas@gmail.com>
2023-03-06 10:50:53 -0600

    1.6 - Lista Optimizada / Mejor func insertFirst

--- TEC1.cpp
+++ TEC1.cpp
@@ -20,36 +20,43 @@ class Collector {
      int data; // data del nodo
      Node* nextPtr; // puntero al siguiente nodo
 
-     Node(){
+     Node(){ //constructor sin data
         data = 0;
         nextPtr = nullptr;
      }
-
      Node (int x){ // constructor con data
         data = x;
         nextPtr = nullptr;
      }
-
-     void setData(int x){ // setear data
+     Node (int x, Node* sig){ //constructor con data y ptr al siguiente
         data = x;
+        nextPtr = sig;
      }
 
-     void setNext(Node* x){
-        nextPtr = x;
-     }
-     int getData(){ // obtener data
-        return data;
-     }
-     Node* getNexPtrt() {
-        return nextPtr;
-     }
-
+     void setData(int x);
+     void setNextPtr(Node* x);
+     int getData();
+     Node* getNextPtr();
 };
+
+void Node::setData(int x){
+    data = x;
+}
+void Node::setNextPtr(Node* x){
+    nextPtr = x;
+}
+int Node::getData(){
+    return data;
+}
+Node* Node::getNextPtr(){
+    return nextPtr;
+}
+
 
 class List{
     public:
         Node* headPtr; // puntero al head
-        Node* tailPtr;
+        Node* tailPtr; //puntero al final
         int size;
 
         List(){ // constructor, siempre sin nodos
@@ -58,94 +65,94 @@ class List{
             size = 0;
         }
 
-        void setHead(int data){
-            if (size != 0){
-                 headPtr->setData(data);
-            }
-            else{
-                cout << "Lista no posee elementos" << endl;
-            }
-        }
+        void setHeadData(int data);
+        int getHeadData();
+        void insertLast(int data);
+        void insertFirst(int data);
+        void deleteItem(int data);
+};
 
-        int getHead(){
-            if (size != 0){
-                 return headPtr->getData();
-            }
-            else{
-                cout << "Lista no posee elementos" << endl;
-            }
+void List::setHeadData(int data){
+    if (size != 0){
+        headPtr->setData(data);
+    }
+    else{
+        cout << "Lista no posee elementos" << endl;
+    }
+}
+int List::getHeadData(){
+    if (size != 0){
+        return headPtr->getData();
+    }
+    else{
+        cout << "Lista no posee elementos" << endl;
+        return -404;
+    }
+}
+void List::insertLast(int data){
+    if (false != false){
+        "piringola";
+    }
+    else{
+        Node* newPtr = new Node(data);
+        if (headPtr == nullptr){
+            headPtr = newPtr;
+            tailPtr = newPtr;
+            size++;
         }
-
-        void insertLast(int data){
+        else{
+            tailPtr->setNextPtr(newPtr);
+            tailPtr = tailPtr->getNextPtr();
+            size++;
+        }
+    }
+}
+void List::insertFirst(int data){
+    if (false != false){
+        "piringola";
+        /*
+        newPtr = newcollector.retrievePtr() // *pepe;
+        *newPtr = new Node (data);
+        if (headPtr == nullptr){
+            headPtr = newPtr;
+            tailPtr = newPtr;
+            size++;
+        }
+        else {
+            newPtr->setNextPtr(headPtr);
+            headPtr = newPtr;
+            size++;
+        }
+        */
+    }
+    else{
+        if (headPtr == nullptr){
             Node* newPtr = new Node(data);
-            if (false != false){
-                "piringola";
-            }
-            else{
-                if(headPtr == nullptr){
-                    headPtr = newPtr;
-                    tailPtr = newPtr;
-                    size++;
-
-                }
-                else{
-                     tailPtr->setNext(newPtr);
-                     tailPtr = tailPtr->getNexPtrt();
-                     size++;
-
-                }
-            }
+            headPtr = newPtr;
+            tailPtr = newPtr;
+            size++;
         }
-
-        void insertFirst(int data){
-            if (false != false) {
-                "piringola";
-                /*
-                newPtr = newcollector.retrievePtr() / *pepe;
-                *newPtr = new Node (69);
-                if (headPtr == nullptr){
-                    headPtr = newPtr;
-                    tailPtr = newPtr;
-                    size++;
-                }
-                else {
-                    newPtr->setNext(headPtr);
-                    headPtr = newPtr;
-                    size++;
-                }
-                */ 
-            }
-            else {
-                Node* newPtr = new Node(data);
-                if (headPtr == nullptr){
-                    headPtr = newPtr;
-                    tailPtr = newPtr;
-                    size++;
-                }
-                else {
-                    newPtr->setNext(headPtr);
-                    headPtr = newPtr;
-                    size++;
-                }
-            }
+        else{
+            Node* newPtr = new Node(data, headPtr);
+            headPtr = newPtr;
+            size++;
         }
+    }
 
-        void deleteItem(int data) {
-
-        }
-
-
-    };
+}
+void List::deleteItem(int data){
+    "w";
+}
 
 int main(){
 
     List nuevaLista;
-   nuevaLista.insertFirst(99);
-   nuevaLista.insertFirst(100);
-   nuevaLista.insertFirst(101);
-   cout << nuevaLista.headPtr->getData() << endl;
-   cout << nuevaLista.headPtr->nextPtr->getData() << endl;
-   cout << nuevaLista.headPtr->nextPtr->nextPtr->getData() << endl;
+    nuevaLista.insertFirst(99);
+    nuevaLista.insertFirst(100);
+    nuevaLista.insertFirst(101);
+    cout << nuevaLista.headPtr->getData() << endl;
+    cout << nuevaLista.headPtr->nextPtr->getData() << endl;
+    cout << nuevaLista.headPtr->nextPtr->nextPtr->getData() << endl;
 
 
     return 0;
